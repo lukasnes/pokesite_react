069636382db02c8b896d99e75f1f241c3158e336
Lukas <lukasnes.work@gmail.com>
2022-08-03 16:23:25 -0500

    Added new sidebar icons

--- src/images/Images.js
+++ src/images/Images.js
@@ -2,6 +2,12 @@ import Bulbasaur from './articles/bulbasaur.png';
 import TradingCardGame from './articles/pokemon_tcg.jpeg';
 import PokemonTeam from './articles/poketeam.jpg';
 import HomeIcon from './icons/home.png';
+import DexIcon from './icons/dex.png';
+import TeamIcon from './icons/team.png';
+import BattleIcon from './icons/battle.png';
+import ForumIcon from './icons/forum.png';
+import PagesIcon from './icons/pages.png';
+import AboutIcon from './icons/about.png';
 import PokeLogo from './logos/poke_logo.png';
 import PokeBG from './logos/pokemon_bg.png';
 
@@ -13,7 +19,13 @@ const Images = {
         team: PokemonTeam
     },
     icons: {
-        home: HomeIcon
+        home: HomeIcon,
+        dex: DexIcon,
+        team: TeamIcon,
+        battle: BattleIcon,
+        forum: ForumIcon,
+        pages: PagesIcon,
+        about: AboutIcon
     },
     logos: {
         logo: PokeLogo,
